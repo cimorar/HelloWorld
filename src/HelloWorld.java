7dc8adc2fa46ad0c2641d91594eacb0cb7f4f20a
radim <radim@localhost>
2016-11-15 12:53:58 +0100

    update 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// updated on 18/11/2016
+		// updated on 20/11/2016
 		System.out.println("Hello world from Radim");
-		
+		System.out.println("Hello world from Radim");		
 	}
-
 }
